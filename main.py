afdd5f07642ba920da3c45a5e934ed66667c80f0
obrunorf <obrunorf@gmail.com>
2025-05-05 19:51:35 -0400

    moves around
    vuahsssssh

--- main.py
+++ main.py
@@ -3,17 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    pygame.display.set_caption('Asteroids')
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    spaceship = Player(x, y)
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         screen.fill("black")
+        dt = clock.tick(60) / 1000
+        spaceship.update(dt)
+        spaceship.draw(screen)
+        pygame.display.flip()
 
     print("Starting Asteroids!")
     print("Screen width: "+str(SCREEN_WIDTH))
